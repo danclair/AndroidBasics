6dc67d2b23a9da7b011cea336cbc59d7e24ae389
Dan Clair <dan@omniasalud.com>
2021-03-03 10:49:36 -0600

    new commit

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { Button, Text, View} from 'react-native';
+import { 
+  Button, Text, TextInput,
+  Image, ActivityIndicator,
+  Platform, StyleSheet, View, ProgressBarAndroid 
+} from 'react-native';
+import axios from 'axios';
+
+
 export default class App extends React.Component {
-  state = {number: 0};
-   render() {
-       return (
-        <View style={{flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          padding: 20
-        }}>
-          <Button title='     –     '
-                  color='#e57373'
-                  onPress={() => this.decrement()}/>
-          <Text>Value = {this.state.number}</Text>
-          <Button title='     +     '
-                  color='#64B5F6'
-                  onPress={() => this.increment()}/>
-        </View>
-       );
-   }
-   decrement() {
-     this.setState({number: this.state.number - 1});
-   }
-   increment() {
-     this.setState({number: this.state.number + 1});
-   }
+  state = {text: '', loading: false, error: null, imgUrl: null}
+  return (
+    <View style={styles.containerStyle}>
+    <TextInput
+        style={styles.textInputStyle}
+        placeholder="Enter text to search an image"
+        returnKeyType='search'
+        autoFocus={true}
+        onChangeText{(text) => this.setState({text})}
+        onSubmitEditing={() => this.searchPicture()}/>
+    onChangeText={(text) => this.setState({text})}
+    onSubmitEditing={() => this.searchPicture()}
+    {
+        this.state.imgUrl &&
+        <Image
+            source={{uri: this.state.imgUrl}}
+            style={{flex: 1}}/>
+    }
+    </View>
+  );
+
+  searchPicture() {
+    this.setState({loading: true, error: null, imgUrl: null});
+    axios.get('https://www.reddit.com/r/pics/search.json', {
+      params: {
+        restrict_sr: 'on',
+        limit: 1,
+        sort: 'new',
+        q: this.state.text
+      }
+    }).then(response => {
+      this.setState({
+        imgUrl: response.data.data.children[0]
+            .data.preview.images[0].source.url,
+        error: null, loading: false
+      })
+    }).catch(error => {
+      this.setState({error: error.message, loading: false, imgUrl: null})
+    })
+  }
 }
